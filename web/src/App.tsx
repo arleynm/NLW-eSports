86db67f853c69cd65900f834be81629445d89dee
arleynm <arleyaugusto5@gmail.com>
2022-09-18 22:31:22 -0300

    Finalização do mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import * as Dialog from '@radix-ui/react-dialog'
 import { GameController } from 'phosphor-react';
+import { Input } from './components/Form/input';
 
 
 
@@ -59,49 +60,97 @@ function App() {
 
           <Dialog.DialogContent className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480] shadow-lg shadow-black/25'>
             <Dialog.Title className='text-3xl text-white font-black'>Publique um anuncio</Dialog.Title>
-                <form className="mt-8">
+                <form className="mt-8 flex flex-col gap-4">
                   <div className="flex flex-col gap-2">
                     <label htmlFor="game" className='font-semibold'>Qual seu nome?</label>
-                    <Input t/>
+                    <Input id= "game" placeholder="Selecione o game que deseja jogar"/>
                   </div>
-                  <div>
+                  <div className='flex flex-col gap-2'>
                     <label htmlFor='name'> Seu nome (ou nickname)</label>
-                    <input id="name" placeholder="Como te chama dentro do game?"></input>
-                  </div>
-                  <div>
-
-                    <div>
-                    <label htmlFor='yearsPlaying'> Joga há quanto temnpo</label>
-                    <input id="yearsPlaying" placeholder="Tudo bem ser zero?"></input>
-                    </div>
-                    <div>
-                    <label htmlFor='discord'> Qual seu Discord?</label>
-                    <input id="discord" placeholder="Usuario#0000?"></input>
-                    </div>
+                    <Input id="name" placeholder="Como te chama dentro do game?"/>
                   </div>
 
-                  <div>
-                    <div>
+
+                  <div className="grid grid-cols-2 gap-6">
+                    <div className="flex flex-col gap-2">
+                      <label htmlFor='yearsPlaying'> Joga há quanto temnpo</label>
+                      <Input id="yearsPlaying" placeholder="Tudo bem ser zero?"/>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <label htmlFor='discord'> Qual seu Discord?</label>
+                      <Input id="discord" placeholder="Usuario#0000?"/>
+                    </div>
+                  </div>
+
+                  <div className="flex gap-6">
+                    <div className='flex flex-col gap-2'>
                     <label htmlFor="weekDays">Quando costuma jogar?</label>
+
+                    <div className='grid grid-cols-4 gap-2'>
+                      <button 
+                      title='Domingo'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        
+                        D
+                      </button>
+                      <button 
+                      title='Segunda'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        S
+                      </button>
+                      <button 
+                      title='Terça'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        T
+                      </button>
+                      <button 
+                      title='Quarta'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        Q
+                      </button>
+                      <button 
+                      title='Quinta'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        Q
+                      </button>
+                      <button 
+                      title='Sexta'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        S
+                      </button>
+                      <button 
+                      title='Sabado'
+                      className='w-8 h-8 bg-zinc-900'
+                      >
+                        S
+                      </button>
+                      </div>
                     </div>
-                    <div>
+
+                    <div className='flex flex-col gap-2 flex-1'>
                       <label htmlFor="hourStart">Quando horário do dia?</label>
-                      <div>
-                        <input id="hourStart" type ="time" placeholder='De'/>
-                        <input id="hourEnd" type ="time" placeholder='Até'/>
+                      <div className='grid grid-cols-2 gap-2'>
+                        <Input id="hourStart" type ="time" placeholder='De'/>
+                        <Input id="hourEnd" type ="time" placeholder='Até'/>
                       </div>
                     </div>
                   </div>
 
-                <div>
+                <div className='mt-2 flex gap-2 text-sm'>
                   <input type="checkbox" />
                   Costumome conectar ao chat de voz 
                 </div>
 
-                <footer>
-                  <button>Cancelar</button>
-                  <button type='submit'>
-                    <GameController/>
+                <footer className='mt-4 flex justify-end gap-4'>
+                  <Dialog.Close type="button" className='bg-zinc-500 px-5 h-12 rounded-md font-semibold'>Cancelar</Dialog.Close>
+                  <button type='submit'className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 houver:bg-zinc-600 hover:bg-violet-600'>
+                    <GameController className='w-6 h-6'/>
                   </button>
                 </footer>
                 </form>
--- web/src/components/Form/input.tsx
+++ web/src/components/Form/input.tsx
@@ -3,8 +3,10 @@ import {InputHTMLAttributes} from 'react'
 interface InputProps extends InputHTMLAttributes<HTMLPictureElement>{}
 
 export function Input (props: InputProps){
-    <input
-    {...props}
-    className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder: text-zinc-900"
-    />
+    return(
+        <input
+        {...props}
+        className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder: text-zinc-500"
+        />
+    )
 }
